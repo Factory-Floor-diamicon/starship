1279c8cb721fdb19ab35ab74cc8b5665e7a0fd59
George Williams <george412williams@gmail.com>
2020-09-03 22:48:04 -0500

    expanding objects and extending vehicle class next add flying vehicle class

---
+++ src/Car.java
@@ -0,0 +1,52 @@
+public class Car extends Vehicle{
+
+    //instead of strs, use super method like Jay for practice
+
+    private int milesPerGallon;
+
+    public void depoyAirBag(){
+        System.out.println("Deploying airbag...");
+    }
+
+    public Car(){
+
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println("Cranking engine...");
+    }
+
+    public void genericTurnOn(){
+        //System.out.println("Vehicle is turning on...");
+        super.turnOn();//this is the parent reference
+    }
+
+    public Car(boolean isOperable, int passengerCapacity, String powerSource, int milesPerGallon) {
+        super(isOperable, passengerCapacity, powerSource);
+        this.milesPerGallon = milesPerGallon;
+    }
+
+    public Car(int milesPerGallon) {
+        this.milesPerGallon = milesPerGallon;
+    }
+
+    //gtrs/strs
+
+
+    public int getMilesPerGallon() {
+        return milesPerGallon;
+    }
+
+    public void setMilesPerGallon(int milesPerGallon) {
+        this.milesPerGallon = milesPerGallon;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "milesPerGallon=" + milesPerGallon +
+                '}';
+    }
+
+}
