1ef364ad0b0cf719a6cb0054f7bc75f07007fd59
George Williams <george412williams@gmail.com>
2020-09-10 20:15:38 -0500

    adding modal manual js and html

---
+++ src/main/java/io/factoryjunkyard/js/mouse-events.js
@@ -0,0 +1,15 @@
+(function () {
+"use strict";
+
+
+$('#toggle').click(function () {
+    $(this).css({
+       'background-image': 'url(img/metal-panels.jpeg',
+       'background-size': 'cover'
+    });
+});
+
+
+
+
+});
